d41b4c4f87cce3e6bd52a93a6d773c3f8418019a
SeolMoon <miunseol@gmail.com>
2023-12-06 09:52:29 +0900

    img 참조 실패

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import Vuex from 'vuex'
+// import Vuex from 'vuex'
 
 // Vue.use(Vuex)
 
--- src/views/ProductBoard.vue
+++ src/views/ProductBoard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="product-board">
     <!-- <div>{{$route.params.category}}</div> -->
-    <product-category :category="category"/>
+    <product-category :category="category" />
     <product-item class="product-item" :product="product" />
     <!-- <div>{{product[1].name}}</div> -->
     <!-- <app-paging :pageSetting="pageDataSetting(total, limit, block, this.page)"
@@ -10,48 +10,47 @@
 </template>
 
 <script>
-import ProductCategory from '@/components/ProductCategory.vue';
-import ProductItem from '@/components/ProductItem.vue';
-import data from '@/assets/data/data.json';
+import ProductCategory from "@/components/ProductCategory.vue";
+import ProductItem from "@/components/ProductItem.vue";
+import data from "@/assets/data/data.json";
 // import AppPaging from '@/components/AppPaging.vue';
 
 const product = data;
 
 export default {
-  components: { ProductCategory, ProductItem,
+  components: {
+    ProductCategory,
+    ProductItem,
     // AppPaging
-   },
-   props:{
-    category:{
-      type : String,
+  },
+  props: {
+    category: {
+      type: String,
       required: true,
-    }
+    },
   },
-  data(){
-    return{
+  data() {
+    return {
       product,
-      
-    }
+    };
   },
-  
-  
-}
+};
 </script>
 
 <style scoped>
-  .product-board{
-    margin: auto;
-    width: 1200px;
-    /* height: 1200px; */
-    height: 1800px;
-  }
-  .product-item{
-    width: 910px;
-    height: 1115px;
-    outline:1px solid red;
-    display: flex;
-    justify-content: space-between;
-    flex-wrap: wrap;
-    margin: auto;
-  }
+.product-board {
+  margin: auto;
+  width: 1200px;
+  /* height: 1200px; */
+  height: 1800px;
+}
+.product-item {
+  width: 910px;
+  height: 1115px;
+  outline: 1px solid red;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: auto;
+}
 </style>
