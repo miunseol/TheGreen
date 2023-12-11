1a863d3c69754ab7b3591d2cd789251a5348556c
kikihong <ghdtlr29@naver.com>
2023-12-11 17:01:43 +0900

    주문완료

---
+++ src/views/OrderComplete.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div></div>
+    <div>
+      <div>
+        <div 주문이 정상적으로 완료되었습니다>
+          <div></div>
+          <div></div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+          <div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div 결제정보>
+          <div></div>
+          <div>
+            <div></div>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div>
+            <h4>주문금액</h4>
+          </div>
+          <div>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <div>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
